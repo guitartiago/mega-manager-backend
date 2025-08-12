30d7cf32aef0ba55dc4dd5564e2ea0bb82fdd1bb
Tiago Correia <guitartiago@gmail.com>
2025-08-12 17:01:30 -0300

    correção config estoque e produto

--- src/main/java/com/megamanager/produto/adapter/config/ProdutoUseCaseConfig.java
+++ src/main/java/com/megamanager/produto/adapter/config/ProdutoUseCaseConfig.java
@@ -3,21 +3,32 @@ package com.megamanager.produto.adapter.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
-import com.megamanager.estoque.application.port.in.RegistrarEntradaUseCase;
-import com.megamanager.estoque.application.port.out.EntradaEstoqueRepository;
-import com.megamanager.estoque.application.usecase.EntradaEstoqueService;
+import com.megamanager.produto.application.port.in.CadastrarProdutoUseCase;
+import com.megamanager.produto.application.port.in.ListarProdutosUseCase;
+import com.megamanager.produto.application.port.out.ProdutoRepository;
+import com.megamanager.produto.application.usecase.CadastrarProdutoService;
+import com.megamanager.produto.application.usecase.ListarProdutosService;
 
 @Configuration
 public class ProdutoUseCaseConfig {
-	
-	@Bean
-	public EntradaEstoqueService estradaEstoqueService(EntradaEstoqueRepository entradaEstoqueRepository) {
-		return new EntradaEstoqueService(entradaEstoqueRepository);
-	}
-	
-	@Bean
-	public RegistrarEntradaUseCase registrarEntradaUseCase(EntradaEstoqueService service) {
-		return service;
-	}
 
+    @Bean
+    public CadastrarProdutoService produtoService(ProdutoRepository produtoRepository) {
+        return new CadastrarProdutoService(produtoRepository);
+    }
+    
+    @Bean
+    public ListarProdutosService listarProdutoService(ProdutoRepository produtoRepository) {
+        return new ListarProdutosService(produtoRepository);
+    }
+
+    @Bean
+    public CadastrarProdutoUseCase cadastrarProdutoUseCase(CadastrarProdutoService service) {
+        return service;
+    }
+
+    @Bean
+    public ListarProdutosUseCase listarProdutosUseCase(ListarProdutosService service) {
+        return service;
+    }
 }
